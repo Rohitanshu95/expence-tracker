c4ebafec6e1c11583d6d3194f1f029893485afb2
rohitanshu95 <rohitanshudhar86@gmail.com>
2026-05-06 21:47:01 +0530

    backend cookies set to samesite:none

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -74,6 +74,7 @@ const login = async (req, res) => {
     res.cookie('token', token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
+      sameSite: 'none',
       maxAge: 3600000 // 1 hour
     });
 
